e5ec48bcf36e4d30ada14c5d6ef902989d333ec8
alinoo4ka <alinacoder2@gmail.com>
2024-11-27 14:07:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+from handlers import start_handler
+
+API_ID = int(os.environ.get('20045757', ''))
+API_HASH = os.environ.get('7d3ea0c0d4725498789bd51a9ee02421', '')
+BOT_TOKEN = os.environ.get('6954388145:AAHx9TnuzOmw3Ae4RE4EHPPEMUzD1Pgz5kY', '')
+
+
+app = Client(
+    session_name="DJwjndk",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+app.add_handler(start_handler)
+
+print("Бот запущен!")
+app.run()
---
+++ start_handler.py
@@ -0,0 +1,7 @@
+from pyrogram import filters
+from pyrogram.handlers import MessageHandler
+
+def start(client, message):
+    message.reply_text('добро пожаловать в первый нвполненный багов бот котрый вряьли комуто будет нужен')
+
+start_handler = MessageHandler(start, filters.command("start"))
